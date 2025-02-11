260d3e031a17908c80de33a313f194dab5e2d06c
haised <h4i.se7@gmail.com>
2025-02-11 19:50:51 +0800

    Edit the contact me

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -49,7 +49,8 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1 }}
-          href='#contact'
+          href='https://linktr.ee/mrkivan29'
+          target='_blank'
           className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'
         >
           contact me <Image src={assets.right_arrow_white} alt='' className='w-4' />
